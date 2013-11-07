bdf5faa84217176292fb56e706aa1885d70c920c
Susan Prestage <susan.prestage@gmail.com>
2013-11-06 22:58:38 -0800

    chapter 5

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,8 +6,36 @@ class QuotesController < Rulers::Controller
 
   def a_quote
     render :a_quote, :noun => :winking
-    # "There is nothing either good or bad " +
-    #   "but thinking makes it so." +
-    #   "\n<pre>\n#{env}\n</pre>"
   end
+
+  def quote_1
+    quote_1 = Rulers::Model::FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
+  def update_quote
+    attrs = {
+      "submitter" => "web user"
+      "quote" => "A picture is worth a thousand words",
+      "attribution" => "anonymous"
+    }
+    m = FileModel.edit attrs
+    render :quote, :obj => m
+  end
+
 end
